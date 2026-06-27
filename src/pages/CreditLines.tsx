eaa89da59f6df099c12200f770c0957d122ac8eb
Callora Developer <dev@callora.io>
2026-06-27 19:38:29 +0100

    feat: add repayment plan visualizer to CreditLines detail

--- src/pages/CreditLines.tsx
+++ src/pages/CreditLines.tsx
@@ -1,6 +1,7 @@
 import { useState, useMemo } from 'react';
 import { Link } from 'react-router-dom';
 import { StatusBadge } from '../components/StatusBadge';
+import { RepaymentPlanChart } from '../components/RepaymentPlanChart';
 import { MOCK_CREDIT_LINES } from '../data/mockData';
 import type { CreditLineStatus, SortField, SortDirection } from '../types/creditLine';
 import {
@@ -76,6 +77,10 @@ function CreditLineCard({ line }: { line: typeof MOCK_CREDIT_LINES[0] }) {
         {line.utilized > 0 && (
           <button className="cl-action-btn repay">↙ Repay</button>
         )}
+      </div>
+
+      <div className="cl-card-detail">
+        <RepaymentPlanChart line={line} />
       </div>
     </div>
   );
